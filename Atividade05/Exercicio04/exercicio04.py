9b45443ba5bd5ee0f40c96b73be29f787c7cc56b
Pedro Haupenthal <pedro.haupenthal@gmail.com>
2020-05-15 01:16:48 -0300

    Adicao atividade 05

---
+++ Atividade05/Exercicio04/exercicio04.py
@@ -0,0 +1,13 @@
+# Exercicio 04
+# Pedro Afonso Ferreira Haupenthal 823974
+
+# OBS
+# (content)
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+!wget "https://raw.githubusercontent.com/PedroHaupenthal/Image-Processing/master/Atividade04/Exercicio01/image.jpg" -O "image.jpg"
+
+img1 = cv.imread("image.jpg")
