15eebe9582463a4dd9f65be10007c75db9eebe76
Pedro Haupenthal <pedro.haupenthal@gmail.com>
2020-05-19 12:14:34 -0300

    Finalizacao exercicio 03 Atividade 05

--- Atividade05/Exercicio03/exercicio03.py
+++ Atividade05/Exercicio03/exercicio03.py
@@ -2,6 +2,33 @@
 # Pedro Afonso Ferreira Haupenthal 823974
 
 # OBS
-# Para este exercicio, foi utililzada uma biblioteca exclusiva do Google Colab, logo, o mesmo se encontra no link abaixo
+# 1º O algoritmo so pode ser executado no Google Colab devido a importações específicas
+# 2º Video capturado com a camera frontal do celular e utilizado o software online clideo.com para redimensionar, passando de 1980x1080 para 600x600
+# e ajustar a proporção desejada de 18:9 para 1:1
 
-# https://colab.research.google.com/drive/1jojmRuq-Pr44aX10WwqDw7JcE5ne8U08?usp=sharing
+from google.colab.patches import cv2_imshow as colab_imshow
+import numpy as np
+import cv2 as cv
+
+!wget "https://github.com/PedroHaupenthal/Image-Processing/blob/master/Atividade05/base_files/eu.mp4?raw=true" -O "eu.mp4"
+!wget "https://raw.githubusercontent.com/PedroHaupenthal/Image-Processing/master/Atividade05/base_files/cascades/haarcascade_frontalface_default.xml" -O "haarcascade_frontalface_default.xml"
+
+video = cv.VideoCapture("eu.mp4")
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while(True):
+    _, frame = video.read()
+
+    img1 = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img1, 
+                                          1.3, 
+                                          5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(frame, 
+                      (x ,y),
+                      (x + w, y + h),
+                      (255, 200, 0), 10)
+        roi_im1 = img1[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+    colab_imshow(frame)
