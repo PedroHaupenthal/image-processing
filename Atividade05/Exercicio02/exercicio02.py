d46a684f9c0b5f20241100dee987a9b45edaa549
Pedro Haupenthal <pedro.haupenthal@gmail.com>
2020-05-19 11:54:16 -0300

    Adicao exercicio 2 e correcao do 1

--- Atividade05/Exercicio02/exercicio02.py
+++ Atividade05/Exercicio02/exercicio02.py
@@ -1,13 +1,31 @@
 # Exercicio 02
 # Pedro Afonso Ferreira Haupenthal 823974
 
-# OBS
-# (content)
-
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-!wget "https://raw.githubusercontent.com/PedroHaupenthal/Image-Processing/master/Atividade04/Exercicio01/image.jpg" -O "image.jpg"
+!wget "https://github.com/PedroHaupenthal/Image-Processing/blob/master/Atividade05/base_files/eu.jpg" -O "eu.jpg"
+!wget "https://raw.githubusercontent.com/PedroHaupenthal/Image-Processing/master/Atividade05/base_files/cascades/haarcascade_eye.xml" -O "haarcascade_eye.xml"
 
-img1 = cv.imread("image.jpg")
+
+img1 = cv.imread('eu.jpg')
+img1 = cv.cvtColor(img1,cv.COLOR_BGR2RGB)
+img2 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+
+eye_cascade = cv.CascadeClassifier("haarcascade_eye.xml")
+
+res = eye_cascade.detectMultiScale(img2, 
+                                   scaleFactor = 1.25, 
+                                   minNeighbors = 5)
+
+if res is not None:
+  for (x, y, larg, alt) in res:
+    img1 = cv.rectangle(img1, 
+                        (x, y),
+                        (x + larg, y + alt), 
+                        (0, 200, 0), 15)
+
+plt.figure(figsize=(15,15))
+plt.imshow(img1), plt.axis("off")
+plt.show()
